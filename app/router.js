ccc95664e05d05eeba033a4ad94cc79ec099dfb9
Andrew Akers <andrewrdakers@gmail.com>
2014-11-18 17:37:44 -0800

    initial commit

--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
-import config from './config/environment';
-
 var Router = Ember.Router.extend({
-  location: config.locationType
 });
 
 Router.map(function() {
+  this.resource('characters', function() {
+    this.resource('character', { path: '/:character_id' });
+  });
 });
 
 export default Router;
